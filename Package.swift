1fe7a47aa45be83d483853b105f485cb6d5a2d15
Andrew Paterson <Andrewjp97@me.com>
2025-10-25 10:25:06 -0400

    Initial commit of PlaidAPI Swift package generated from OpenAPI specification.

---
+++ Package.swift
@@ -0,0 +1,48 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PlaidAPI",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "PlaidAPI",
+            type: .dynamic,
+            targets: ["PlaidAPI"]
+        ),
+        .library(
+            name: "PlaidAPIStatic", 
+            type: .static,
+            targets: ["PlaidAPI"]
+        ),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+    .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.6.0"),
+    .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.7.0"),
+    .package(url: "https://github.com/apple/swift-openapi-urlsession", from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "PlaidAPI",
+            dependencies: [
+                .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
+                .product(name: "OpenAPIURLSession", package: "swift-openapi-urlsession")
+            ],
+            plugins: [
+                .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator"),
+            ]
+        ),
+    ]
+    
+)
